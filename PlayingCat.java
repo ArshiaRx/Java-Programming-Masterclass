c7b759271e1e4ddd725035f8a85632ec7ebdda02
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-11-26 03:18:49 -0500

    Update PlayingCat.java

--- PlayingCat.java
+++ PlayingCat.java
@@ -1,3 +1,9 @@
+//The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive).
+//Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+//Write a method isCatPlaying that has 2 parameters. Method needs to return true if the cat is playing, otherwise return false
+//1st parameter should be of type boolean and be named summer it represents if it is summer.
+//2nd parameter represents the temperature and is of type int with the name temperature.
+
 public class PlayingCat {
     public static boolean isCatPlaying(boolean summer, int temperature) {
         if ((summer) && (temperature > 24 && temperature < 46)) {
