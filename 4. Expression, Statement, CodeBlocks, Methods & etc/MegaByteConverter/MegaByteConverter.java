69979532b7216ee59500a2e31c7d09ee1c354352
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-12-18 22:17:02 -0500

    Add files via upload

---
+++ 4. Expression, Statement, CodeBlocks, Methods & etc/MegaByteConverter/MegaByteConverter.java
@@ -0,0 +1,16 @@
+public class MegaByteConverter {
+    public static void main(String args[]){
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+    public static void printMegaBytesAndKiloBytes (int kiloBytes){
+        int MegaBytes = kiloBytes / 1024;
+        int remaining = kiloBytes % 1024;
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }
+        else {
+        System.out.println(kiloBytes + " KB = " + MegaBytes + " MB and " + remaining + " KB");
+    }
+}}
