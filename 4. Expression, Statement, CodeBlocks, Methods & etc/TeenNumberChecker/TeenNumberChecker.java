1dbc11d82e2a6901485aeaa85e04f8504dce07da
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-12-21 19:05:20 -0500

    Update TeenNumberChecker.java

--- 4. Expression, Statement, CodeBlocks, Methods & etc/TeenNumberChecker/TeenNumberChecker.java
+++ 4. Expression, Statement, CodeBlocks, Methods & etc/TeenNumberChecker/TeenNumberChecker.java
@@ -7,7 +7,7 @@ public class TeenNumberChecker {
         System.out.println(isTeen(9));
         System.out.println(isTeen(13));
     }
-    public static boolean hasTeen(int age1, int age2, int age3) {// int num3) {
+    public static boolean hasTeen(int age1, int age2, int age3) {
         if ((age1 >= 13 && age1 <= 19) || (age2 >= 13 && age2 <= 19) || (age3 >= 13 && age3 <= 19)){
             return true;
         }
