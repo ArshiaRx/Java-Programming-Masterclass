32d49e2a3cfe87b0587ff99a4981fd958fd49d2b
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-12-28 00:18:46 -0500

    Create DayOfTheWeek.java

---
+++ 5. Control Flow Statements/DayOfTheWeek/DayOfTheWeek.java
@@ -0,0 +1,42 @@
+public class DayOf_TheWeek {
+    public static void main (String[] args){
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(6);
+    }
+    public static void printDayOfTheWeek (int day){
+
+        switch(day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+                default:
+                    System.out.println("Invalid day");
+
+        }
+    }
+}
