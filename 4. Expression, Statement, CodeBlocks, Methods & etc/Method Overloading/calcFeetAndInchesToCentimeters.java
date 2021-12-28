a569a973986fbf4a580cd30dd7c078a41b1b3fa2
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-12-27 23:21:23 -0500

    Update calcFeetAndInchesToCentimeters.java

--- 4. Expression, Statement, CodeBlocks, Methods & etc/Method Overloading/calcFeetAndInchesToCentimeters.java
+++ 4. Expression, Statement, CodeBlocks, Methods & etc/Method Overloading/calcFeetAndInchesToCentimeters.java
@@ -27,11 +27,10 @@ public class calcFeetAndInchesToCentimeters {
             if (inches < 0){
                 return -1;
             }
-
+            
             double feet = (int) inches / 12;    // foot is 12 inches, feet is 1 inch
             double remainingInches = (int) inches % 12;
             System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches);
             return calcFeetAndInchesToCentimeter(feet, inches);
         }
-
     }
